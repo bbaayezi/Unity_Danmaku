490d54f148f5c3ddbb224c44f2a33a721ea8e424
bbaayezi@woobest <zhour@woobest.com>
2018-08-01 13:59:04 +0800

    start writing round shooting bullet

---
+++ BulletRoundController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletRoundController : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// transform.parent.transform.rotation *= Quaternion.Euler(0, 0, 1);
+	}
+
+	private void OnEnable() 
+	{
+		BulletRoundSpawner.OnSpawnFinished += NavigateBullets;
+	}
+
+	private void OnDisable() 
+	{
+		BulletRoundSpawner.OnSpawnFinished -= NavigateBullets;
+	}
+
+	private void NavigateBullets()
+	{
+		Quaternion rotation = transform.parent.transform.rotation;
+		transform.rotation = rotation;
+		// move forward
+		// bullet.transform.position += new Vector3(0, -1, 0);
+		InvokeRepeating("Shooting", .06f, 0.1f);
+		InvokeRepeating("ClearBullets", 2f, 2f);
+	}
+
+
+	void Shooting()
+	{
+		transform.localPosition += new Vector3(0, 1f, 0) * Time.deltaTime;
+	}
+	void ClearBullets()
+	{
+		Destroy(gameObject);
+	}
+}
---
+++ BulletRoundSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletRoundSpawner : MonoBehaviour 
+{
+	public delegate void FinishedEvent();
+	public static event FinishedEvent OnSpawnFinished;
+	public GameObject bullet;
+	GameObject[] positioningGroup;
+	public GameObject parent;
+	private int frameCount = 0;
+	// Use this for initialization
+	void Start () 
+	{
+		positioningGroup = GameObject.FindGameObjectsWithTag("RoundBullets");
+	}
+	
+	void FixedUpdate () 
+	{
+		// rotation
+		transform.GetChild(0).transform.rotation *= Quaternion.Euler(0, 0, 1);
+		frameCount ++;
+		if (frameCount == 3)
+		{ 
+			Instantiate(bullet, transform.position, Quaternion.Euler(0, 0, 0), parent.transform);
+			if (OnSpawnFinished != null)
+			{
+				OnSpawnFinished();
+			}
+			frameCount = 0;
+		}
+	}
+}
